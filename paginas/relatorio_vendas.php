b6462419f698d42aae8e75dc62ef801ad629854a
Vanderley Guilherme Manchini <147321920+Drakko-VGM@users.noreply.github.com>
2025-06-06 23:22:27 -0300

    Adição de PDF
    Adicionei os PDF's e modifiquei as páginas com os botões corretos para o acesso do relatório de produtos em estoque e de vendas por funcionário

--- paginas/relatorio_vendas.php
+++ paginas/relatorio_vendas.php
@@ -5,12 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Relatório de vendas</title>
     <link rel="icon" type="image/png" href="/Loja-de-roupa/assets/img/logo_ME.png">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/pdfStyle.css">
 </head>
-<body>
+    <div class="container my-4"><br>
+        <div class="chart-icon left">
+            <a href="home.php"><img src="../assets/img/voltar.svg" alt="Voltar"></a>
+        </div>
+        <div class="chart-icon right form-form">
+            <a href="graficos.php" data-toggle="modal"><img src="../assets/img/chart.svg" alt="Gráfico" style="width:45px;"></a>
+            <a href="pdfProduto.php" data-toggle="modal"><img src="../assets/img/pdf.svg" alt="PDF Produtos em Estoque" style="width:45px;"></a>
+        </div>
+        <h1 class="text-center">Relatório de vendas</h1>
+    </div>
 
-    <h1 style="color:#3C3E40;">Relatório de vendas</h1>
-    <a href="home.php"><img src="../assets/img/voltar.svg" alt="Voltar"></a><br><br><br>
-    <a href="graficos.php" data-toggle="modal"><img src="../assets/img/chart.svg" alt="Gráfico" style="width:35px;"></a>
-</div>
 </body>
 </html>
